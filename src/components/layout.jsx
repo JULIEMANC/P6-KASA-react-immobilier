202cead76f644ce15bce898b38859b7f7867edc9
JULIEMANC <136808377+JULIEMANC@users.noreply.github.com>
2023-10-31 11:39:00 +0100

    Collapses work + :active sur nav work
    Collapses sur housing et about work

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,27 +1,53 @@
-
-import React from 'react';
-import {Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink} from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const [activeLink, setActiveLink] = useState(false);
+  const toggleLink = () => {
+    setActiveLink(!activeLink);
+  };
   return (
-    <div className='body'>
+    <div className="body">
       <header>
-      <img className="logoheader"src='../assets/logo/LOGO.png'alt="logo header"/>
-      <nav className='navbar'>
-        <Link className='navlink' to="/">Accueil</Link>
-        <Link className='navlink'to="/about">A Propos</Link>
-      </nav>
+        <img
+          className="logoheader"
+          src="../assets/logo/LOGO.png"
+          alt="logo header"
+        />
+        <nav>
+          <ul>
+            <li className={activeLink ? 'active' : ''}>
+              <NavLink
+                onClick={toggleLink}
+                className="navlink"
+                to="/"
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li className={activeLink ? 'active' : ''}>
+              <NavLink
+                onClick={toggleLink}
+                className="navlink"
+                to="/about"
+              >
+                A Propos
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
       <footer>
-        <img className="logofooter"src='../assets/logo/logo-footer.png'alt="logo footer de kasa"/>
-        <p className='footerletter'>© 2020 Kasa. All rights reserved</p>
-     
-      </footer> 
+        <img
+          className="logofooter"
+          src="../assets/logo/logo-footer.png"
+          alt="logo footer de kasa"
+        />
+        <p className="footerletter">© 2020 Kasa. All rights reserved</p>
+      </footer>
     </div>
-  )
+  );
 };
 
 export default Layout;
